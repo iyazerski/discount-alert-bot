407ef83ea9aad79f68e98f0802898bf257309fa9
Ihar Yazerski <ihar.yazerski@gmail.com>
2024-11-18 16:59:24 +0100

    - create Dockerfile for bot_interface; - minor bugfixes;

--- bot_interface/processors.py
+++ bot_interface/processors.py
@@ -3,7 +3,7 @@ from bot_common.database import Database
 from bot_interface.bot import Bot
 from bot_interface.settings import BotInterfaceSettings
 
-settings = BotInterfaceSettings()
+settings = BotInterfaceSettings()  # noqa
 bot = Bot(bot_token=settings.TELEGRAM_BOT_TOKEN)
 broker = Broker(dsn=settings.broker_dsn)
-db = Database(dsn=settings.db_dsn, **settings.db_connection_params)
+db = Database(**settings.db_connection_params)
--- bot_interface/settings.py
+++ bot_interface/settings.py
@@ -1,7 +1,5 @@
-from pydantic_settings import BaseSettings
-
 from bot_common.settings import CommonSettings
 
 
-class BotInterfaceSettings(BaseSettings, CommonSettings):
+class BotInterfaceSettings(CommonSettings):
     TELEGRAM_BOT_TOKEN: str
